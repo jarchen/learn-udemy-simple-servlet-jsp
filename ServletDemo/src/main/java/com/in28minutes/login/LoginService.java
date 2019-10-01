9f9391772802b8263b807627f6106b90c5666663
Jargo Chen <>
2019-10-01 14:19:00 +0800

    Update LoginService

--- ServletDemo/src/main/java/com/in28minutes/login/LoginService.java
+++ ServletDemo/src/main/java/com/in28minutes/login/LoginService.java
@@ -2,10 +2,6 @@ package com.in28minutes.login;
 
 public class LoginService {
     public boolean isUserValid(String user, String password) {
-        if (user.equals("abc") && password.equals("123")) {
-            return true;
-        }
-
-        return false;
+        return user.equals("abc") && password.equals("123");
     }
 }
